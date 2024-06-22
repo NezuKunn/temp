b927079011b51283e2cce0490f90b65ff8e4f813
NezuKunn <137272488+NezuKunn@users.noreply.github.com>
2024-06-22 23:42:32 +0700

    Update temp.py

--- temp.py
+++ temp.py
@@ -3,26 +3,13 @@ import requests
 
 app = Flask(__name__)
 
-API_KEYS = ['aa-aa-00-aa', 'aa-00-aa-aa']
-
-@app.route('/proxy1', methods=['POST'])
+@app.route('/raydium_data', methods=['POST'])
 def proxy():
-    api_key = request.headers.get('X-API-KEY')
-    if api_key not in API_KEYS:
-        return {'error': 'Недействительный API ключ'}, 403
-
-    url = f"http://localhost:8899{request.full_path}"
-    response = requests.request(
-        method=request.method,
-        url=url,
-        headers={key: value for (key, value) in request.headers.items() if key != 'Host'},
-        data=request.data,
-    )
+    print(request.json())
 
     return Response(
-        response=response.content,
-        status=response.status_code,
-        headers=dict(response.headers),
+        response={"d": "a"},
+        status=200
     )
 if __name__ == '__main__':
-    app.run(host='144.202.63.224', port=3014)
+    app.run(host='185.231.155.69', port=8014)
